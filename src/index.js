dd05adef5c4b6ed8f9776a4d84c45679cd596e50
djballowe <djballowe@gmail.com>
2022-06-06 18:47:38 -0700

    js loads up on start

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-function makeHome() {
-    const content = document.querySelector('#content');
+const test = require('./load')
 
-    const elementFactory = (type, attributes, text) => {
-        const create = document.createElement(type);
-        create.textContent = text;
-        for (key in attributes) {
-            create.setAttribute(key, attributes[key]);
-        }
-        return create;
-    }
-
-    const container = elementFactory('div', {id: 'homepage'});
-    content.appendChild(container);
-    
-    const homepage = document.querySelector('#homepage');
-    homepage.appendChild(elementFactory('div', {id: 'background'}));
-    
-    const text = document.querySelector('#background');
-    text.appendChild(elementFactory('h1',{class: 'text'}, 'EVERYONE'));
-    text.appendChild(elementFactory('h1',{class: 'text'}, 'WANTS'));
-    text.appendChild(elementFactory('h1',{class: 'text'}, 'IN'));
-    text.appendChild(elementFactory('button', {class: 'button'}, 'Get Started'));
-}
+window.onload = test;
