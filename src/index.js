edc33cdae6bc0e15dd80bceb5960ae787d150685
djballowe <djballowe@gmail.com>
2022-06-07 14:42:02 -0700

    creating clear page function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import { makeMenu } from "./menu"
 import { makeContact } from "./contact"
 
 
-makeContact();
+document.querySelector('header').addEventListener('click', function(e) {
+    const nav = e.target.getAttribute('id');
+    if (nav === 'H') {
+        makeHome();
+    } else if (nav === 'M') {
+        makeMenu();
+    } else if (nav === 'C') {
+        makeContact();
+    }
+})
 
 
 
